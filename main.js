d2374e8ea9e22d9c1bb7a456ffb9cd98cbada462
DnD07 <07dnd.dahal@gmail.com>
2023-08-15 07:02:54 -0500

    img

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ document.querySelector('#search-icon').onClick = () => {
     search.classList.toggle('active');
 }
 
+
+document.querySelector('#login-btn').onClick = () =>{
+    document.querySelector('.login-form-container').classList.toogle('active');
+}
